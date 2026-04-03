27760cc556f00620a19a9882c6ba23bf632207b8
Pragya-19 <138296470+Pragya-19@users.noreply.github.com>
2026-04-03 11:04:00 +0530

    Added API response validation

--- api_clients/user_client.py
+++ api_clients/user_client.py
@@ -1,6 +1,14 @@
 import requests
 
-BASE_URL = "https://reqres.in/api"
+BASE_URL = "https://jsonplaceholder.typicode.com"
 
 def get_users():
-    return requests.get(f"{BASE_URL}/users?page=2")
+    return requests.get(f"{BASE_URL}/users")
+
+def create_user():
+    payload = {
+        "name": "Pragya",
+        "username": "pragya19",
+        "email": "pragya@example.com"
+    }
+    return requests.post(f"{BASE_URL}/users", json=payload)
--- tests/api/test_users.py
+++ tests/api/test_users.py
@@ -1,5 +1,20 @@
-import requests
+from api_clients.user_client import get_users, create_user
 
 def test_get_users():
-    response = requests.get("https://reqres.in/api/users?page=2")
+    response = get_users()
     assert response.status_code == 200
+
+    data = response.json()
+    assert isinstance(data, list)
+    assert len(data) > 0
+    assert "name" in data[0]
+    assert "email" in data[0]
+
+def test_create_user():
+    response = create_user()
+    assert response.status_code == 201
+
+    data = response.json()
+    assert data["name"] == "Pragya"
+    assert data["username"] == "pragya19"
+    assert data["email"] == "pragya@example.com"
