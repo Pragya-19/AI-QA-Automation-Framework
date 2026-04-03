428f932029e6dc2ae050ef94630a0af496d8525f
Pragya-19 <138296470+Pragya-19@users.noreply.github.com>
2026-04-03 11:33:13 +0530

    Added negative API tests

---
+++ tests/api/test_negative.py
@@ -0,0 +1,15 @@
+import requests
+
+BASE_URL = "https://jsonplaceholder.typicode.com"
+
+def test_invalid_endpoint():
+    response = requests.get(f"{BASE_URL}/invalid")
+    assert response.status_code == 404
+
+def test_create_user_missing_field():
+    payload = {
+        "name": "Pragya"
+    }
+    response = requests.post(f"{BASE_URL}/users", json=payload)
+
+    assert response.status_code == 201
